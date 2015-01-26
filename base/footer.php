024b3322dab1c2935e26c0f555b7099a65a3782d
Felimon Gayanilo <felimon.gayanilo@tamucc.edu>
2015-01-26 17:18:09 -0600

    Standard GCOOS Footer
    Footer elements

---
+++ base/footer.php
@@ -0,0 +1,64 @@
+       <hr />
+            <table width="100%" border="0" cellspacing="6" cellpadding="0" >
+        <tr>
+	        <td colspan="4"><a href="http://www.ioos.noaa.gov" title="Integrated Ocena Observing System" target="_blank" style="color:#069">Integrated Ocean Observing System (IOOS)</a> </td>
+        </tr>
+        <tr align="left" valign="top">
+          <td width="23%" style="color:#006; border-right:thin; border-right-style:solid"><p style="background:#006; color:#FFF"  align="center"><strong>Regional Coastal Observing Systems</strong></p>
+          <p><a href="http://www.aoos.org/" title="Alaska Ocean Observing System" target="_blank" style="color:#069">Alaska</a><br />
+          <a href="http://cara.uprm.edu/" title="Caribbean Coastal Ocean Observing System" target="_blank" style="color:#069">Caribbean</a><br />
+          <a href="http://www.cencoos.org" title="Central and Northern California Ocean Observing" target="_blank" style="color:#069">Central and Northern California</a><br />
+		  <a href="http://glos.us/" title="Great Lakes Observing System&quot;&gt;Great Lakes" target="_blank" style="color:#069">Great Lakes Observing System</a><br />
+          <a href="http://gcoos.org/" title="Gulf of Mexico Coastal Ocean Observing System" target="_blank" style="color:#069">Gulf of Mexico</a><br />
+          <a href="http://www.pacioos.org" title="Pacific Islands Ocean Observing System" target="_blank" style="color:#069">Pacific Islands</a><br />
+          <a href="http://maracoos.org" title="Mid-Atlantic Regional Association Coastal Ocean Observing System" target="_blank" style="color:#069">Mid-Atlantic</a><br />
+          <a href="http://www.neracoos.org" title="Northeastern Regional Association of Coastal Ocean Observing Systems" target="_blank" style="color:#069">Northeast-Atlantic</a><br />
+          <a href="http://www.nanoos.org" title="Pacific Northwest Regional Ocean Observing System" target="_blank" style="color:#069">Pacific Northwest</a><br />
+          <a href="http://www.sccoos.org/" title="Southern California Coastal Ocean Observing System" target="_blank" style="color:#069">Southern California</a><br />
+          <a href="http://secoora.org" title="Southeast Coastal Ocean Observing Regional Association" target="_blank" style="color:#069">Southeast-Atlantic</a><br />
+
+          <br />
+          <a href="http://www.usnfra.org/" title="National Federation of Regional Associations for Coastal and Ocean Observing" target="_blank" style="color:#069">National Federation of Regional Associations<br />
+for Coastal and Ocean Observing</a><br />
+          
+          </p></td>
+          <td width="25%" style="color:#006; border-right:thin; border-right-style:solid"><p style="background:#006; color:#FFF"  align="center"><strong>National Observing System Partners</strong></p>
+          <p style="color:#039"><a href="http://www.act-us.info/" title="Alliance for Coastal Technologies ACT" target="_blank" style="color:#069">Alliance for Coastal Technologies (ACT)</a><br />
+            <a href="http://www.sura.org/home/" title="Southeastern Universities Research Association SURA" target="_blank" style="color:#069">Southeastern Universities Research Association (SURA)</a></p>
+          <p style="color:#039"><a href="http://www.ndbc.noaa.gov/" target="_blank" style="color:#069">National Data Buoy Data Center (NDBC)</a><br />
+            <a href="http://oceanservice.noaa.gov/" target="_blank" style="color:#069">National Ocean Service (NOS)</a><br />
+            <a href="http://www.nerrs.noaa.gov" target="_blank" style="color:#069">National Estuarine Research Reserve System (NERRS)</a><br /><br />
+            
+            <a href="http://www.ioos.noaa.gov/qartod/welcome.html" target="_blank" style="color:#069">Quality Assurance of Real Time Ocean Data (QUARTOD)</a>
+            </p>
+          </td>
+          <td width="31%" style="color:#006; border-right:thin; border-right-style:solid"><p style="background:#006; color:#FFF"  align="center"><strong>GCOOS Local Data Nodes</strong></p>
+            <p style="color:#039"><a href="http://www.disl.org/" title="Dauphin Island Sea lab DISL" target="_blank" style="color:#069">Dauphin Island Sea Laboratory (DISL)</a><br />
+            <a href="http://lighthouse.tamucc.edu/TCOON/HomePage" title="Texas Coastal Ocean Observation Network TCOON" target="_blank" style="color:#069">Texas Coastal Ocean Observation Network (TCOON)</a><br />
+            <a href="http://www.mote.org/" title="Mote Marine Laboratory MOTE" target="_blank" style="color:#069">Mote Marine Laboratory (MOTE)</a><br />
+            <a href="http://comps.marine.usf.edu/" title="Coastal Ocean Monitoring and Prediction System COMPS" target="_blank" style="color:#069">Coastal Ocean Monitoring and Prediction System (COMPS)</a><br />
+            <a href="http://www.cengoos.org/" title="Central Gulf Ocean Observing System CenGOOS" target="_blank" style="color:#069">Central Gulf Ocean Observing System (CenGOOS)</a><br />
+            <a href="http://www.lumcon.edu/" title="Louisiana University Marine Consortium LUMCON" target="_blank" style="color:#069">Louisiana University Marine Consortium (LUMCON)</a><br />
+            <a href="http://wavcis.csi.lsu.edu/" title="Wave-Current-Surge Information System WAVCIS" target="_blank" style="color:#069">Wave-Current-Surge Information System (WAVCIS)</a><br />
+            <a href="http://tabs.gerg.tamu.edu/tglo" title="Texas Automated Buoy System TABS" target="_blank" style="color:#069">Texas Automated Buoy System (TABS)</a><br />
+            <a href="http://recon.sccf.org/" title="Sanibel-Captiva Conservation Foundation SCCF RECON" target="_blank" style="color:#069">Sanibel-Captiva Conservation Foundation (SCCF RECON)</a><br />
+            <a href="http://coaps.fsu.edu/gcoos/" title="Center for Ocean-Atmospheric Prediction Studies FSU/COAPS" target="_blank" style="color:#069">Center for Ocean-Atmospheric Prediction Studies (FSU/COAPS)</a><br /> 
+            <a href="http://www.myfwc.com/" title="Florida Fish and Wildlife Conservation Commission FWRI" target="_blank" style="color:#069">Florida Fish and Wildlife Conservation Commission (FWRI)</a><br /> 
+
+          </p>          
+          </td>
+          <td width="21%" style="color:#006; border-right:thin; border-right-style:solid"><p style="background:#006; color:#FFF"  align="center"><strong>GCOOS Links</strong></p>
+          <p style="color:#039"><a href="http://gcoos.org/" title="GCOOS Home Page" target="_blank" style="color:#069">GCOOS Home</a><br />
+            <a href="http://products.gcoos.org/" title="GCOOS Products Page" target="_blank" style="color:#069">GCOOS Products Page</a><br />
+            <a href="http://data.gcoos.org/" title="GCOOS Data Portal" target="_blank" style="color:#069">GCOOS Data Portal</a><br />
+          </p>
+          <p style="color:#039"><a href="http://www.tamu.edu/statements/privacy.html" title="Privacy Statement" target="_blank" style="color:#069">Privacy and Security Statement</a><br />
+            <a href="http://itim.tamu.edu/dmca.html" title="Legal Notices" target="_blank" style="color:#069">Legal Notices</a></p>
+          <p style="color:#039"> <a href="mailto:srmartin@tamu.edu" style="color:#069">Contact GCOOS Home Webmaster</a><br />
+            <a href="mailto:shinichi@tamu.edu" style="color:#069">Contact GCOOS Products Page Webmaster</a><br />
+            <a href="mailto:felimon.gayanilo@tamucc.edu" style="color:#069">Contact GCOOS Data Portal Webmaster</a></p></td>
+        </tr>
+      </table>
+      <p align="left">Copyright © 2008-2015 All rights reserved. Gulf of Mexico Coastal Ocean Observing System (GCOOS), College Station, Texas, USA
+              <!-- end #footer -->	
+              
